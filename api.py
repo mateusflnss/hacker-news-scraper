0c076b6974beb16c78a69bdb91ca1baabfccef90
Mateusflns <mateusfelipe.do.nascimento@gmail.com>
2026-08-19 21:27:22 -0300

    Add GET /stories/{story_id}

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ from crud import add_story
 from db import engine, init_db, get_session
 from model import Story
 from sqlmodel import Session, select
-from fastapi import FastAPI, Depends, Query
+from fastapi import FastAPI, Depends, Query, HTTPException
 
 
 init_db()
@@ -27,3 +27,17 @@ def list_stories(
     query = query.offset(offset).limit(limit)
 
     return session.exec(query).all()
+
+
+@app.get("/stories/{story_id}")
+def get_story(story_id: int,
+              session: Session = Depends(get_session),
+
+              ):
+    query = select(Story)
+    query = query.where(Story.hn_id == story_id)
+    item = session.exec(query).all()
+    if item == None:
+        raise HTTPException(status_code = 404, detail={"message" : f"no item with {story_id} id"})
+
+    return item
